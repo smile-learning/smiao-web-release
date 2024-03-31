724ddfc2b39fd7fa745c09947f1146c7f29047f2
shafreeck <shafreeck@gmail.com>
2024-03-31 18:15:42 +0800

    Add all files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,144 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let defaultImages = ['windows-icon-v2.svg', 'mac-icon.svg', 'android-icon.svg', 'smiao-icon.svg'];
+	let titles = ['Windows 版', 'macOS 版', 'Android 版', 'Web 版'];
+	let descriptions = [
+		'支持 Windows 10 及 Windows 11',
+		'支持 macOS 10.11 及以上版本',
+		'支持 Android 8.0 及以上系统',
+		'直接打开网页使用'
+	];
+	let links = [
+		'download/smiao-v1.0.0.msi',
+		'download/smiao-v1.0.0.dmg',
+		'download/smiao-v1.0.0.apk',
+		'https://smiao.art'
+	];
+	let images = defaultImages.slice();
+	function mouseEnter(i: number) {
+		images[i] = 'download-icon-v2.svg';
+		if (i === 3) {
+			images[i] = 'logo.png';
+		}
+	}
+	function mouseLeave(i: number) {
+		images[i] = defaultImages[i];
+	}
+</script>
+
+<div class="page-container">
+	<div class="header-container">
+		<img src="logo.png" alt="logo" />
+		<div>喵喵静听</div>
+	</div>
+
+	<div class="content-container">
+		<div class="title-container">
+			<h1>喵喵静听下载</h1>
+			<p>同时支持 Android，Windows， macOS 和 Web 版本</p>
+		</div>
+
+		<div class="download-container">
+			{#each images as image, i}
+				<div class="apps-item">
+					<a
+						class="apps-item-icon-box"
+						tabindex={i}
+						on:mouseenter={() => mouseEnter(i)}
+						on:mouseleave={() => mouseLeave(i)}
+						href={links[i]}
+					>
+						<img src={image} alt={titles[i]} />
+					</a>
+					<p>{titles[i]}</p>
+					<p class="text-description">{descriptions[i]}</p>
+				</div>
+			{/each}
+		</div>
+	</div>
+	<div class="footer">
+		<p class="text-description">喵喵静听 © 2024，开发者：花心</p>
+	</div>
+</div>
+
+<style>
+	:global(body) :global(html) {
+		height: 100%;
+	}
+	.page-container {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+		background-color: #f4f4fc;
+	}
+	.header-container {
+		margin-top: 20px;
+		margin-left: 20px;
+		display: flex;
+		box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+		border-radius: 10px;
+	}
+	.header-container > img {
+		height: 50px;
+	}
+	.header-container div {
+		display: inline-block;
+		margin-left: 10px;
+		font-size: 1.5em;
+		color: #333;
+		align-self: center;
+	}
+	.content-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		height: 50vh;
+	}
+
+	.title-container {
+		font-size: 1.5em;
+		text-align: center;
+	}
+
+	.download-container {
+		width: 80%;
+		display: flex;
+		margin-top: 50px;
+		align-self: center;
+		justify-content: space-between;
+	}
+	.download-container > div {
+		flex: 1;
+		text-align: center;
+	}
+	.apps-item-icon-box {
+		width: 100px;
+		height: 100px;
+		border: 1px solid rgba(0, 0, 0, 0.05);
+		box-shadow:
+			0 12px 48px rgba(77, 94, 112, 0.08),
+			0 6px 16px rgba(77, 94, 112, 0.04),
+			0 3px 8px rgba(77, 94, 112, 0.02);
+		border-radius: 12px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		cursor: pointer;
+		position: relative;
+		margin: 0 auto;
+	}
+	.apps-item-icon-box img {
+		width: 64px;
+		height: 64px;
+		object-fit: contain;
+	}
+	.text-description {
+		font-size: 0.8em;
+		color: #666;
+	}
+	.footer {
+		width: 80%;
+		margin-top: auto;
+		margin-bottom: auto;
+		align-self: center;
+	}
+</style>
